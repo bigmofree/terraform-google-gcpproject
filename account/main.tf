b2bee53512aa49f3542fa5513ef0afd3adfb3323
Oleksii Honcharenko <mofree@Oleksiis-MacBook-Pro.local>
2024-04-20 16:28:13 -0500

    Updated maint.tf

--- account/main.tf
+++ account/main.tf
@@ -1,8 +1,10 @@
+#Searching for billing account
 data "google_billing_account" "acct" {
   display_name = "My Billing Account"
   open         = true
 }
 
+#Creating unique ID for the project
 resource "random_string" "string" {
   length  = 16
   numeric = false
@@ -11,14 +13,14 @@ resource "random_string" "string" {
   upper   = false
 }
 
-
+#Creating Google project with unique ID and "GCP Project" name
 resource "google_project" "gcpproject" {
   name            = "GCP Project"
   project_id      = random_string.string.result
   billing_account = data.google_billing_account.acct.id
 }
 
-
+#Assigning resources to the project
 resource "null_resource" "set-project" {
   triggers = {
     always_run = "${timestamp()}"
