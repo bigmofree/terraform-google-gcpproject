0588b0e2763d72bdc2bec501e0fcf9064466c88e
moldangrus <160317152+moldangrus@users.noreply.github.com>
2024-04-21 15:04:03 -0700

    Update maint.tf

--- sql/maint.tf
+++ sql/maint.tf
@@ -1,19 +1,18 @@
-#Creating SQL Database instance
+# Creating SQL Database instance
 resource "google_sql_database_instance" "sqlinstance" {
-  name             = "db-instance"
-  database_version = "MYSQL_5_7"
+  name               = "db-instance"
+  database_version   = "MYSQL_5_7"
   deletion_protection = false
-  settings {
-    tier = "us-central1"
-  }
+  tier               = "db-f1-micro"  # or any other appropriate tier
 }
-#Creating DB user
+
+# Creating DB user
 resource "google_sql_user" "user" {
   name     = "oleksii"
   instance = google_sql_database_instance.sqlinstance.name
 }
-#Specifying DB
 
+# Specifying DB
 resource "google_sql_database" "sqldatabase" {
   name     = "wordpress"
   instance = google_sql_database_instance.sqlinstance.name
