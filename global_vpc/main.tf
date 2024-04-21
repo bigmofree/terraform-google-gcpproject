ea9aab8e9318736da329eef4b2ac08dc8f26a7a4
moldangrus <160317152+moldangrus@users.noreply.github.com>
2024-04-21 16:15:39 -0700

    Update main.tf

--- global_vpc/main.tf
+++ global_vpc/main.tf
@@ -2,6 +2,6 @@
 
 resource "google_compute_network" "global_vpc" {
     name = "global-vpc"
-        auto_create_subnetworks = "true"
+        auto_create_subnetworks = true
         routing_mode = "GLOBAL"
 }
