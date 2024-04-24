cf808bb2f3efc4afeb7605e6d50f0c8d77f0ddc0
moldangrus <160317152+moldangrus@users.noreply.github.com>
2024-04-24 15:35:56 -0700

    Update main.tf

--- account/main.tf
+++ account/main.tf
@@ -1,64 +1,75 @@
-data “google_billing_account” “acct” {
-  display_name = “My Billing Account”
+data "google_billing_account" "acct" {
+  display_name = "My Billing Account"
   open         = true
 }
-output “id” {
+
+output "id" {
   value = data.google_billing_account.acct.id
 }
-output “name” {
+
+output "name" {
   value = data.google_billing_account.acct.name
 }
-output “display_name” {
+
+output "display_name" {
   value = data.google_billing_account.acct.display_name
 }
-resource “random_password” “password” {
+
+resource "random_password" "password" {
   length  = 16
   numeric = false
   special = false
   lower   = true
   upper   = false
 }
-#  creates project
-resource “google_project” “testproject” {
-  name            = “gcp project”
+
+# Create project
+resource "google_project" "terraform-google-gcpproject" {
+  name            = "gcp project"
   project_id      = random_password.password.result
   billing_account = data.google_billing_account.acct.id
 }
-# shows project id
-output “project_id” {
-  value = google_project.testproject.id
+
+# Show project id
+output "project_id" {
+  value = google_project.terraform-google-gcpproject.id
 }
-#  sets default project id
-resource “null_resource” “set-project” {
+
+# Set default project id
+resource "null_resource" "set-project" {
   triggers = {
-    always_run = “${timestamp()}”
+    always_run = "${timestamp()}"
   }
-  provisioner “local-exec” {
-    command = “gcloud config set project gcp project”
+  provisioner "local-exec" {
+    command = "gcloud config set project project ${google_project.terraform-google-gcpproject.project_id}"
   }
 }
 
-# enable apis
-resource “google_project_service” “project” {
+# Enable APIs
+resource "google_project_service" "project" {
   count = length(var.apis)
-}
+  
 
-# project = “your-project-id”
+  # Place service configuration here
   service = var.apis[count.index]
 
   disable_dependent_services = true
-    depends_on = [
+
+  depends_on = [
     google_project.testproject,
     null_resource.set-project
   ]
 }
-variable “apis” {
-  type = list(string)
-default = [
-         “compute.googleapis.com”,
-         “dns.googleapis.com”,
-         “storage-api.googleapis.com”,
-         “container.googleapis.com”,
-         “file.googleapis.com”
-]
+
+variable "apis" {
+  type    = list(string)
+  default = [
+    "compute.googleapis.com",
+    "dns.googleapis.com",
+    "storage-api.googleapis.com",
+    "container.googleapis.com",
+    "file.googleapis.com"
+  ]
 }
+
+
